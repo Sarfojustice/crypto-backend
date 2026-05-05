6656bd0d89a20158e89d0a6088278e5ae0ea122a
Justice Osei Sarfo <sarfojustice96@gmail.com>
2026-05-05 22:39:55 +0000

    fix: optimize production cookies with SameSite=None and Secure

--- controllers/authController.js
+++ controllers/authController.js
@@ -10,11 +10,11 @@ const signToken = (id) => {
 const createSendToken = (user, statusCode, res) => {
   const token = signToken(user._id);
   const cookieOptions = {
-    expires: new Date(Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000),
+    expires: new Date(Date.now() + parseInt(process.env.JWT_COOKIE_EXPIRES_IN) * 24 * 60 * 60 * 1000),
     httpOnly: true,
+    sameSite: 'none',
+    secure: true
   };
-  
-  if (process.env.NODE_ENV === 'production') cookieOptions.secure = true;
 
   res.cookie('jwt', token, cookieOptions);
 
